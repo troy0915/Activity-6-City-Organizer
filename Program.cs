1c1b65665d7cb8eba3a612023d5a5124626d6489
troy0915 <johntroydolom7@gmail.com>
2025-08-07 15:14:19 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_6_City_Organizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+            string input;
+
+            Console.WriteLine("Enter city names (type 'exit' to finish):");
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+                cities.Add(input);
+            }
+            var distinctCities = cities.Distinct().ToList();
+            distinctCities.Sort();
+            Console.WriteLine("\nCities in alphabetical order:");
+            foreach (var city in distinctCities)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
+
+
+
+    
+
